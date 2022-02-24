26b8f2c02afed98b3ee1ea2b9e7101b79c7ad416
falguni roy <falguniroy7890@gmail.com>
2022-02-24 11:16:19 +0600

    MY PIANO

---
+++ script.js
@@ -0,0 +1,53 @@
+var Cbleep = new Audio()
+Cbleep.src = "piano-c_C_major.wav";
+
+var Dbleep = new Audio()
+Dbleep.src = "piano-d_D_major.wav";
+
+var Ebleep = new Audio()
+Ebleep.src = "piano-e_E_major.wav";
+
+var Fbleep = new Audio()
+Fbleep.src = "piano-f_F_major.wav";
+
+var Gbleep = new Audio()
+Gbleep.src = "piano-g_G_major.wav";
+
+var Ableep = new Audio()
+Ableep.src = "piano-a_A_major.wav";
+
+var Bbleep = new Audio()
+Bbleep.src = "piano-b_B_major.wav";
+
+var C1bleep = new Audio()
+C1bleep.src = "C1.wav";
+
+var D1bleep = new Audio()
+D1bleep.src = "D1.wav";
+
+var E1bleep = new Audio()
+E1bleep.src = "E1.wav";
+
+var F1bleep = new Audio()
+F1bleep.src = "F1.wav";
+
+var Csbleep = new Audio()
+Csbleep.src = "Cs.wav";
+
+var Dsbleep = new Audio()
+Dsbleep.src = "Ds.wav";
+
+var Fsbleep = new Audio()
+Fsbleep.src = "Fs.wav";
+
+var Gsbleep = new Audio()
+Gsbleep.src = "Gs.wav";
+
+var Bbbleep = new Audio()
+Bbbleep.src = "Bb.wav";
+
+var Cs1bleep = new Audio()
+Cs1bleep.src = "Cs1.wav";
+
+var Ds1bleep = new Audio()
+Ds1bleep.src = "Ds1.wav";
